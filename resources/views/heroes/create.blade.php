0077fd73f5ba8c335c727d7b1d8c390e7b3fd878
mabrural <mabruralmutaqi@gmail.com>
2025-07-14 12:45:09 +0700

    perubahan create dan edit heroes

--- resources/views/heroes/create.blade.php
+++ resources/views/heroes/create.blade.php
@@ -1,56 +1,91 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="container">
-    <div class="page-inner">
-        <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
-            <div>
-                <h3 class="fw-bold mb-3">Create Hero Section</h3>
-                <h6 class="text-muted mb-2">Fill out the form to create your Hero section</h6>
+    <div class="container">
+        <div class="page-inner">
+            <div class="page-header">
+                <h4 class="page-title">Create Hero Section</h4>
+                <ul class="breadcrumbs">
+                    <li class="nav-home">
+                        <a href="{{ route('dashboard') }}">
+                            <i class="flaticon-home"></i>
+                        </a>
+                    </li>
+                    <li class="separator">
+                        <i class="flaticon-right-arrow"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('heroes.index') }}">Hero</a>
+                    </li>
+                    <li class="separator">
+                        <i class="flaticon-right-arrow"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">Create Hero</a>
+                    </li>
+                </ul>
             </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-8">
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-title">Hero Information</div>
+                        </div>
+                        <form action="{{ route('heroes.store') }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="name">Name <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                                id="name" name="name" value="{{ old('name') }}" required>
+                                            @error('name')
+                                                <span class="invalid-feedback">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="subname">Subname</label>
+                                            <input type="text"
+                                                class="form-control @error('subname') is-invalid @enderror" id="subname"
+                                                name="subname" value="{{ old('subname') }}">
+                                            @error('subname')
+                                                <span class="invalid-feedback">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="description">Description</label>
+                                    <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                                        rows="4">{{ old('description') }}</textarea>
+                                    @error('description')
+                                        <span class="invalid-feedback">{{ $message }}</span>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="image">Hero Image</label>
+                                    <input type="file" class="form-control @error('image') is-invalid @enderror"
+                                        id="image" name="image">
+                                    @error('image')
+                                        <span class="invalid-feedback">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="card-action">
+                                <button type="submit" class="btn btn-primary">Save Hero</button>
+                                <a href="{{ route('heroes.index') }}" class="btn btn-danger">Cancel</a>
+                            </div>
+                        </form>
                     </div>
-                @endif
-
-                <form action="{{ route('heroes.store') }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-
-                    <div class="form-group mb-3">
-                        <label for="name" class="form-label">Name <span class="text-danger">*</span></label>
-                        <input type="text" name="name" class="form-control" value="{{ old('name') }}" required>
-                    </div>
-
-                    <div class="form-group mb-3">
-                        <label for="subname" class="form-label">Subname</label>
-                        <input type="text" name="subname" class="form-control" value="{{ old('subname') }}">
-                    </div>
-
-                    <div class="form-group mb-3">
-                        <label for="description" class="form-label">Description</label>
-                        <textarea name="description" rows="4" class="form-control">{{ old('description') }}</textarea>
-                    </div>
-
-                    <div class="form-group mb-3">
-                        <label for="image" class="form-label">Hero Image</label>
-                        <input type="file" name="image" class="form-control">
-                    </div>
-
-                    <button type="submit" class="btn btn-primary">Save Hero</button>
-                    <a href="{{ route('heroes.index') }}" class="btn btn-secondary">Cancel</a>
-                </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
--- resources/views/heroes/edit.blade.php
+++ resources/views/heroes/edit.blade.php
@@ -1,65 +1,100 @@
 @extends('layouts.main')
 
 @section('container')
-<div class="container">
-    <div class="page-inner">
-        <div class="d-flex align-items-left align-items-md-center flex-column flex-md-row pt-2 pb-4">
-            <div>
-                <h3 class="fw-bold mb-3">Edit Hero Section</h3>
-                <h6 class="text-muted mb-2">Update the Hero section of your landing page</h6>
+    <div class="container">
+        <div class="page-inner">
+            <div class="page-header">
+                <h4 class="page-title">Edit Hero Section</h4>
+                <ul class="breadcrumbs">
+                    <li class="nav-home">
+                        <a href="{{ route('dashboard') }}">
+                            <i class="flaticon-home"></i>
+                        </a>
+                    </li>
+                    <li class="separator">
+                        <i class="flaticon-right-arrow"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="{{ route('heroes.index') }}">Hero</a>
+                    </li>
+                    <li class="separator">
+                        <i class="flaticon-right-arrow"></i>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#">Edit Hero</a>
+                    </li>
+                </ul>
             </div>
-        </div>
 
-        <div class="row">
-            <div class="col-md-8">
-                @if ($errors->any())
-                    <div class="alert alert-danger">
-                        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <div class="card-title">Hero Information</div>
+                        </div>
+                        <form action="{{ route('heroes.update', $hero) }}" method="POST" enctype="multipart/form-data">
+                            @csrf
+                            @method('PUT')
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="name">Name <span class="text-danger">*</span></label>
+                                            <input type="text" class="form-control @error('name') is-invalid @enderror"
+                                                id="name" name="name" value="{{ old('name', $hero->name) }}"
+                                                required>
+                                            @error('name')
+                                                <span class="invalid-feedback">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                    <div class="col-md-6">
+                                        <div class="form-group">
+                                            <label for="subname">Subname</label>
+                                            <input type="text"
+                                                class="form-control @error('subname') is-invalid @enderror" id="subname"
+                                                name="subname" value="{{ old('subname', $hero->subname) }}">
+                                            @error('subname')
+                                                <span class="invalid-feedback">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+                                    </div>
+                                </div>
 
-                <form action="{{ route('heroes.update', $hero) }}" method="POST" enctype="multipart/form-data">
-                    @csrf
-                    @method('PUT')
+                                <div class="form-group">
+                                    <label for="description">Description</label>
+                                    <textarea class="form-control @error('description') is-invalid @enderror" id="description" name="description"
+                                        rows="4">{{ old('description', $hero->description) }}</textarea>
+                                    @error('description')
+                                        <span class="invalid-feedback">{{ $message }}</span>
+                                    @enderror
+                                </div>
 
-                    <div class="form-group mb-3">
-                        <label for="name" class="form-label">Name <span class="text-danger">*</span></label>
-                        <input type="text" name="name" class="form-control" 
-                            value="{{ old('name', $hero->name) }}" required>
-                    </div>
-
-                    <div class="form-group mb-3">
-                        <label for="subname" class="form-label">Subname</label>
-                        <input type="text" name="subname" class="form-control" 
-                            value="{{ old('subname', $hero->subname) }}">
-                    </div>
-
-                    <div class="form-group mb-3">
-                        <label for="description" class="form-label">Description</label>
-                        <textarea name="description" rows="4" class="form-control">{{ old('description', $hero->description) }}</textarea>
-                    </div>
-
-                    <div class="form-group mb-3">
-                        <label for="image" class="form-label">Hero Image</label>
-                        @if($hero->image)
-                            <div class="mb-2">
-                                <img src="{{ asset('storage/' . $hero->image) }}" alt="Current Hero Image" width="200">
+                                <div class="form-group">
+                                    <label for="image">Hero Image</label>
+                                    @if ($hero->image)
+                                        <div class="mb-2">
+                                            <img src="{{ asset('storage/' . $hero->image) }}" alt="Current Hero Image"
+                                                width="200">
+                                        </div>
+                                    @endif
+                                    <input type="file" class="form-control @error('image') is-invalid @enderror"
+                                        id="image" name="image">
+                                    <small class="text-muted">Leave empty if you don't want to change the image.</small>
+                                    @error('image')
+                                        <span class="invalid-feedback">{{ $message }}</span>
+                                    @enderror
+                                </div>
                             </div>
-                        @endif
-                        <input type="file" name="image" class="form-control">
-                        <small class="text-muted">Leave empty if you don't want to change the image.</small>
-                    </div>
 
-                    <button type="submit" class="btn btn-primary">Update Hero</button>
-                    <a href="{{ route('heroes.index') }}" class="btn btn-secondary">Cancel</a>
-                </form>
+                            <div class="card-action">
+                                <button type="submit" class="btn btn-primary">Update Hero</button>
+                                <a href="{{ route('heroes.index') }}" class="btn btn-danger">Cancel</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
